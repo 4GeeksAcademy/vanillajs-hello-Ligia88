1ca383ff274efc8b8fba0deb07b035947dea6c94
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-07-03 00:31:36 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  setInterval(() => {
+    const icons = ["♦", "♥", "♠", "♣"];
+    const content = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A",
+    ];
+
+    let randomIcon = Math.floor(Math.random() * icons.length);
+    let randomNumber = Math.floor(Math.random() * content.length);
+    let firstIcon = document.getElementById("firstIcon");
+    let number = document.getElementById("number");
+    let secondIcon = document.getElementById("secondIcon");
+
+    firstIcon.innerHTML = icons[randomIcon];
+    number.innerHTML = content[randomNumber];
+    secondIcon.innerHTML = icons[randomIcon];
+
+    if (
+      (firstIcon.innerHTML == icons[0] && secondIcon.innerHTML == icons[0]) ||
+      (firstIcon.innerHTML == icons[1] && secondIcon.innerHTML == icons[1])
+    ) {
+      firstIcon.style.color = "red";
+      secondIcon.style.color = "red";
+    } else {
+      firstIcon.style.color = "black";
+      secondIcon.style.color = "black";
+    }
+  }, 10000);
 };
